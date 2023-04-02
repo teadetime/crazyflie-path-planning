67f4c75822faac3eb2ad351da0cfd7bd95e2b356
Nathan Faber <ntfaber@gmail.com>
2023-04-02 12:20:04 -0400

    Basic occupancy map functionality

--- src/path_planning/sandbox.py
+++ src/path_planning/sandbox.py
@@ -1,6 +1,16 @@
 """Sandbox environment."""
 
 
-def main(variable: int) -> None:
+import numpy as np
+
+from path_planning.omap import OMap
+
+
+def main() -> None:
     """Main function within Sandbox."""
-    print(f"Hello!{variable}")
+    test_omap = OMap(0.2, 0.2, 0.05)
+
+    # points = np.array([[0, 0, 0], [0.1, 0, 0]])
+    # test_omap.set_points_global(points)
+    test_omap.set_rectangles(np.array([0, 0, 0]), np.array([0.1, 0.1, 0]))
+    print(test_omap.map)
